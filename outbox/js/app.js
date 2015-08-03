cf6343f41e4cc23522b1ab1409efacc56888663f
Romeo Enso <enso123456@gmail.com>
2015-08-03 20:49:29 +0800

    ui.router + directive

--- outbox/js/app.js
+++ outbox/js/app.js
@@ -2,5 +2,21 @@
    'use strict';
 
    angular
-      .module('OutboxApp',[])
+      .module('OutboxApp', [
+         'ui.router'
+      ])
+      .config(function($stateProvider, $urlRouterProvider) {
+         $stateProvider
+         .state('/', {
+            url: '/',
+            controller: 'HomeController',
+            templateUrl: 'views/home.html'
+         })
+         .state('outbox', {
+            url: '/outbox/:id',
+            controller: 'MailController',
+            templateUrl: 'views/outbox.html'
+         })
+         $urlRouterProvider.otherwise('/');
+      })
 })()
--- outbox/js/controllers/MailController.js
+++ outbox/js/controllers/MailController.js
@@ -3,11 +3,17 @@
 
    angular
    .module('OutboxApp')
+   .controller('HomeController', HomeController)
    .controller('MailController', MailController)
 
-   function MailController($scope, emails) {
+   function HomeController($scope, emails) {
       emails.success(function(responseData) {
          $scope.emails = responseData;
       });
+   }
+   function MailController($scope, $stateParams, emails) {
+      emails.success(function(responseData) {
+         $scope.email = responseData[$stateParams.id]
+      })
    }
 })()
